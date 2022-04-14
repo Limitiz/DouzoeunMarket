28d4614e1383a9aefdbea5ab446c21e0e39aec18
Lmitiz <ll55kk678@gmail.com>
2022-04-14 19:38:34 +0900

    feat/DB : DB연동

--- server/server.js
+++ server/server.js
@@ -1,16 +1,43 @@
-const http = require("http");
-const express = require("express");
+import cors from 'cors';
+import express from 'express';
+import mysql from 'mysql';
+import fs from 'fs';
 const app = express();
-const cors = require("cors");
-const port = 8000;
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(cors());
 
 
-app.use(cors({ origin: ['http://localhost:3000', '*'] }));
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
 
+//db 접속
+const conn = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+}, function(err, conn){
+
+    if(err){
+        console.log('접속실패 : ', err);
+        return;
+    }
+    console.log('connect success');
+});
+conn.connect();
+
+
+//홈화면에 user가 출력되는지 확인
 app.get("/", (req, res)=>{
-     res.send({name:"jiyoung"});
+    conn.query(
+         "select * from user",
+         (err, rows, fields) => {
+             res.send(rows);
+         }
+    );
  });
 
-app.listen(port, ()=>{
-    console.log(`server is running on ${port}`);
-})
+app.listen(port, ()=>console.log(`server is running on ${port}`));
