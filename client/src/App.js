69740df34128f0e494cf9df50088a79deca9a4a4
jungmin <minmin1243@naver.com>
2022-04-15 21:34:35 +0900

    feat/Main : 배너 Carousel 및 Card 컴포넌트 생성

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,11 @@ import axios from "axios";
 import { useEffect } from "react";
 import Header from './components/Header';
 import Footer from './components/Footer';
+import MainPage from './components/MainPage';
 
 function App() {
   const callApi = () =>{
-    axios.get("http://localhost:8000").then(res => console.log(res.data));
+    axios.get("http://localhost:8000/main").then(res => console.log(res.data));
   };
 
   useEffect(() => {
@@ -15,11 +16,11 @@ function App() {
 
   return (
     <>
-      <div>test</div>
       <Header />
+      <br/>
+      <MainPage />
       <Footer />
     </>
-    
   );
 }
 
---
+++ client/src/components/Product.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import Card from 'react-bootstrap/Card';
+import axios from 'axios';
+import ProductItem from './ProductItem';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const Product = () => {
+    const[cards, setCards] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async() => {
+            try{
+                const response = await axios.get('http://localhost:8000/main');
+                setCards(response.data);
+            } catch(e){
+                console.log(e);
+            }
+        };
+        fetchData();
+    }, [])
+
+    return(
+        <div>
+            <Row style = {{marginLeft : '5%'}}>
+            {cards.map(deliver => (
+                <Col sm={3}>
+                    <Card style={{ width: '18rem', marginBottom:'2rem'}}>
+                    <Card.Body>
+                        <Card.Title>
+                            <ProductItem key={deliver.idx} deliver={deliver}/>
+                        </Card.Title>
+                    </Card.Body>
+                    </Card>
+                </Col>
+            ))}
+            </Row>
+        </div>
+    )
+}
+
+export default Product;
