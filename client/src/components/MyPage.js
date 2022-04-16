052734646609ea28c7a7495602e2fc8fdf2e014c
Limitiz <ll55kk678@gmail.com>
2022-04-16 21:32:50 +0900

    feat/myPage : ëTab으로 상품 및 찜 목록 ìíTab 생성

--- client/src/components/MyPage.js
+++ client/src/components/MyPage.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {Tabs, Tab} from "react-bootstrap";
 import Profile from './Profile';
+import Product from "./Product";
 
 export default function MyPage(){
 
     return(
-        <>
+        <div style={{width:'80%', margin:'auto'}}>
            <Profile/>
-        </>
+            <Tabs defaultActiveKey="MyProduct" id="uncontrolled-tab-example" className="mb-3">
+                <Tab eventKey="MyProduct" title="나의 상품 목록">
+                    <Product/>
+                </Tab>
+                <Tab eventKey="MyFavorite" title="찜 목록">
+                    <Product/>
+                </Tab>
+            </Tabs>
+        </div>
     )
 }
