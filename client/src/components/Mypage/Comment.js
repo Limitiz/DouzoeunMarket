07bb9e1863f55f5c66de0e1ca75d584a3c637682
Limitiz <ll55kk678@gmail.com>
2022-04-25 03:42:11 +0900

    header까지 수정

--- client/src/components/Mypage/Comment.js
+++ client/src/components/Mypage/Comment.js
@@ -1,15 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import axios from "axios";
+import {useParams} from 'react-router-dom';
+import {useSelector} from "react-redux";
 
 export default function Comment(){
-
+    const {userId} = useParams();
     const base_url = process.env.REACT_APP_BASE_URL;
     const [comments, setComments] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get(`${base_url}/mypage/comments`);
+                const response = await axios.get(`${base_url}/mypage/comments/${userId}`);
                 setComments(response.data);
             } catch (e) {
                 console.log(e);
